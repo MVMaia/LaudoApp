db3eec7fe8c471831c5d40ec91945d779a6af57a
MarceloMaia <marcelo.maia.social99@gmail.com>
2025-08-24 21:58:11 -0300

    Esqueleto da Ui concluída, próx passo: Aplicar responsividade

--- app/src/main/java/com/pressaoeng/laudoapp/MainActivity.kt
+++ app/src/main/java/com/pressaoeng/laudoapp/MainActivity.kt
@@ -1,32 +1,27 @@
 package com.pressaoeng.laudoapp
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.*
 import com.pressaoeng.laudoapp.ui.theme.LaudoAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,33 +42,85 @@ class MainActivity : ComponentActivity() {
                     ){
                         Column(
                             modifier = Modifier.fillMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
                         ) {
+                            //topbar
                             Box(
                                 modifier = Modifier
-                                    .fillMaxWidth(0.3f)
-                                    .fillMaxHeight(fraction = 0.050f),
-                                contentAlignment = Alignment.Center
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .background(Color.White),
+                                contentAlignment = Alignment.Center,
                             ) {
                                 Image(
-                                    painter = painterResource(id = R.drawable.pressaologo),
-                                    contentDescription = "Logo do app",
-                                    modifier = Modifier.fillMaxSize()
+                                    painter = painterResource(id = R.drawable.pressaologowbg),
+                                    contentDescription = "Logo",
+                                    modifier = Modifier
+                                        .width(150.dp)
+                                        .height(150.dp),
                                 )
                             }
                             Box(
                                 modifier = Modifier
-                                    .fillMaxWidth(0.8f)
-                                    .fillMaxHeight(0.9f)
-                                    .padding(innerPadding)
-                                    .background(Color.White, shape = RoundedCornerShape(16.dp))
-                                    .border(2.dp, Color.Black, shape = RoundedCornerShape(16.dp)),
+                                    .fillMaxWidth()
+                                    .fillMaxHeight(),
                                 contentAlignment = Alignment.Center
                             ) {
-                                Greeting(
-                                    name = "Android",
-                                )
+                                //centerBox
+                                Column(
+                                    modifier = Modifier
+                                        .width(325.dp)
+                                        .height(700.dp)
+                                        .background(Color.White, shape = RoundedCornerShape(16.dp))
+                                        .border(2.dp, Color.Black, shape = RoundedCornerShape(16.dp)),
+                                ) {
+                                    Row(
+                                        modifier = Modifier
+                                            .width(325.dp)
+                                            .weight(1f)
+                                            .background(Color.Blue),
+                                    ){
+                                        Text("OLOCOS TEM SOMETHING DIGITADO HERE")
+                                    }
+
+                                    Row(
+                                        modifier = Modifier
+                                            .width(325.dp)
+                                            .weight(1f)
+                                            .background(Color.Red),
+                                    ){
+                                        Text("OLOCOS TEM SOMETHING DIGITADO HERE")
+                                    }
+
+                                    Row(
+                                        modifier = Modifier
+                                            .width(325.dp)
+                                            .weight(1f)
+                                            .background(Color.Green),
+                                    ){
+                                        Text("OLOCOS TEM SOMETHING DIGITADO HERE")
+                                    }
+
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .weight(1f),
+                                        horizontalArrangement = Arrangement.Center,
+                                        verticalAlignment = Alignment.CenterVertically,
+                                    ){
+                                        Button(
+                                            onClick = {},
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .fillMaxHeight()
+                                                .padding(horizontal = 15.dp, vertical = 60.dp),
+                                            shape = RoundedCornerShape(15.dp),
+                                        ) {
+                                            Text(
+                                                text = "Butão"
+                                            )
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
